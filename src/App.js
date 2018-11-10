5b375f60247aedc7a46fd2b9082dd14ed7f3cbc2
LEONARDO HENRIQUES DA LUZ COSTA <029161023@labinfo.unifacs.br>
2018-11-10 15:25:01 -0300

    Criando topbar

--- src/App.js
+++ src/App.js
@@ -1,30 +1,23 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import Button from '@material-ui/core/Button';
+import ReactDOM from 'react-dom';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+};
+
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <h1 style={{color: 'purple'}} >Leo-Senpai</h1>
-          <h1 className="App-link">TA CALOR, DESGRAÇA</h1>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    return <div>ola</div>
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
